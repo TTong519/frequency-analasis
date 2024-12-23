92dc89b99775251bbd8fb72f2aca098b0fd13c28
tyler.tong <tyler.tong@GMRMLTV>
2024-12-23 12:05:29 -0800

    Add project files.

---
+++ uncaesarshift/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.Serialization;
+using System.Text;
+
+namespace frequencyanalasis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt = File.ReadAllText("bible.txt");
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in txt)
+            {
+                if (c < ' ' || c > 'ÿ') continue;
+                sb.Append(c);
+            }
+            txt = sb.ToString();
+            Console.WriteLine("pls type text to frequency analasis");
+            int[] arr = new int[255];
+            foreach(var c in txt)
+            {
+                arr[(int)c]++;
+            }
+            for(int i  = 0; i < arr.Length; i++)
+            {
+                Console.Write((char)(i) + "-");
+                Console.WriteLine(arr[i] + "," + ((float)arr[i]/txt.Length)*100 + "%");
+            }
+        }
+    }
+}
