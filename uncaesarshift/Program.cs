8cb2a6535c054a19ff6b06c36f6e6296cc635dd7
tyler.tong <tyler.tong@GMRMLTV>
2024-12-24 12:30:25 -0800

    rewq

--- uncaesarshift/Program.cs
+++ uncaesarshift/Program.cs
@@ -2,6 +2,9 @@
 using System.Text;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.ComponentModel.DataAnnotations;
 
 namespace frequencyanalasis
 {
@@ -20,105 +23,73 @@ namespace frequencyanalasis
         }
         static void Main(string[] args)
         {
-            float[,] counts = new float[25, 255];
-            int[] matches = new int[25];
-            string txt = File.ReadAllText("J. K. Rowling - Harry Potter 1 - Sorcerer's Stone.txt");
+            float[,] counts = new float[26, 255];
+            int[] matches = new int[26];
+            string plaintext = File.ReadAllText("J. K. Rowling - Harry Potter 1 - Sorcerer's Stone.txt");
             StringBuilder sb = new StringBuilder();
-            foreach (char c in txt)
+            foreach (char c in plaintext)
             {
-                if (c < ' ' || c > 'ÿ') continue;
+                if (c < ' ' || c > 'z') continue;
                 sb.Append(c);
             }
-            txt = sb.ToString();
-            txt = "The late afternoon sun hung low in the sky as Harry and Hagrid made their way back down Diagon Alley, back through the wall, back through the Leaky Cauldron, now empty. Harry didn't speak at all as they walked down the road; he didn't even notice how much people were gawking at them on the Underground, laden as they were with all their funny-shaped packages, with the snowy owl asleep in its cage on Harry's lap. Up another escalator, out into Paddington station; Harry only realized where they were when Hagrid tapped him on the shoulder.";
-            string txt1 = File.ReadAllText("J. K. Rowling - Harry Potter 1 - Sorcerer's Stone.txt");
-            StringBuilder sb1 = new StringBuilder();
-            foreach (char c in txt1)
+            plaintext = sb.ToString();
+            plaintext = "The late afternoon sun hung low in the sky as Harry and Hagrid made their way back down Diagon Alley, back through the wall, back through the Leaky Cauldron, now empty. Harry didn't speak at all as they walked down the road; he didn't even notice how much people were gawking at them on the Underground, laden as they were with all their funny-shaped packages, with the snowy owl asleep in its cage on Harry's lap. Up another escalator, out into Paddington station; Harry only realized where they were when Hagrid tapped him on the shoulder.".ToUpper();
+            string txt1 = File.ReadAllText("words_alpha.txt");
+            plaintext = Sad(plaintext);
+            HashSet<string> dictionary = new HashSet<string>();
+            string[] strings = txt1.Split((char)13);
+            foreach(string s in strings)
             {
-                if (c < ' ' || c > 'ÿ') continue;
-                sb.Append(c);
+                dictionary.Add(s);
             }
-
-            txt1 = sb1.ToString();
-            Random rand = new Random();
-            int N = 5;
             string ciphertext = "";
-            for (int i = 0; i < txt.Length; i++)
+            for (int i = 0; i < plaintext.Length; i++)
             {
-                if (txt[i] == ' ')
+                if (plaintext[i] == ' ')
                 {
-                    ciphertext += txt[i];
+                    ciphertext += plaintext[i];
                     continue;
                 }
-                int num = txt[i] - 'A';
-                num += N;
+                int num = plaintext[i] - 'A';
+                num += 5;
                 num %= 26;
                 ciphertext += (char)(num + 65);
             }
-            int[] arr = new int[255];
-            float[] arr1 = new float[255];
-            foreach(var c in txt1)
+            Console.WriteLine(ciphertext);
+            int N = 0;
+            for (; N < 26; N++)
             {
-                arr[(int)c]++;
-            }
-            for(int i  = 0; i < arr.Length; i++)
-            {
-                Console.Write((char)(i) + "-");
-                Console.WriteLine(arr[i] + "," + ((float)arr[i]/txt1.Length)*100 + "%");
-                arr1[i] = ((float)arr[i] / txt1.Length) * 100;
-            }
-            for (int i = 0; i < 25; i++)
-            {
-                string pt = "";
-                for (int j = 0; j < txt.Length; j++)
+                string daf = "";
+                for (int i = 0; i < plaintext.Length; i++)
                 {
-                    if (ciphertext[i] == ' ')
+                    if (plaintext[i] == ' ')
                     {
-                        pt += ciphertext[i];
+                        daf += plaintext[i];
                         continue;
                     }
-                    int num = txt[i] - 'A';
-                    num -= i+1;
+                    int num = plaintext[i] - 'A';
+                    num += N;
                     num %= 26;
-                    pt += (char)(num + 65);
+                    daf += (char)(num + 65);
                 }
-                foreach (var c in pt)
+                string[] words = daf.Split(' ');
+                foreach (string word in words)
                 {
-                    counts[i,(int)c]+=1;
-                }
-                for (int j = 0; j < 25; j++)
-                {
-                    for(int k = 0; k < 255; k++)
+                    if(dictionary.Contains(word))
                     {
-                        counts[j,k] = ((float)counts[j,k] / txt.Length) * 100;
+                        matches[N]++;
                     }
                 }
             }
-            for (int i = 0; i < 25; i++)
+            int max = matches.Max();
+            for(int i = 0; i < matches.Length; i++)
             {
-                for (int j = 0; j < 255; j++)
+                if (matches[i] == max)
                 {
-                    if (counts[i, j] > arr1[64] - 1 && counts[i, j] < arr1[j] + 1)
-                    {
-                        matches[i]++;
-                    }
+                    Console.WriteLine(i);
+                    break;
                 }
             }
-            int correctval = matches.Max();
-            string plaintext = string.Empty;
-            for (int j = 0; j < ciphertext.Length; j++)
-            {
-                if (ciphertext[j] == ' ')
-                {
-                    plaintext += ' ';
-                    continue;
-                }
-                int num = ciphertext[j] - 'A';
-                num -= correctval;
-                num %= 26;
-                plaintext += (char)(num + 65);
-            }
-            Console.WriteLine(plaintext);
         }
     }
 }
